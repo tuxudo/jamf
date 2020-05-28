4a12ddabde685a308a73056f37bc1b088f46f6f0
tuxudo <tvxudo@gmail.com>
2020-05-28 09:46:13 -0400

    Move to jsonView()

--- jamf_controller.php
+++ jamf_controller.php
@@ -13,13 +13,23 @@ class jamf_controller extends Module_controller
         // No authentication, the client needs to get here
         // Store module path
         $this->module_path = dirname(__FILE__);
+
+        // Add local config
+        configAppendFile(__DIR__ . '/config.php');
     }
 
     public function index()
     {
         echo "You've loaded the Jamf module!";
     }
-    
+
+    // Add the admin page
+    public function admin()
+    {
+        $obj = new View();
+        $obj->view('jamf_admin', [], $this->module_path.'/views/');
+    }
+
     /**
     * Get enrolled via DEP info
     *
@@ -27,14 +37,14 @@ class jamf_controller extends Module_controller
     **/
     public function get_enrolled_via_dep()
     {
-        $obj = new View();
-        $queryobj = new Jamf_model();
         $sql = "SELECT  COUNT(CASE WHEN `enrolled_via_dep` = '1' THEN 1 END) AS Yes,
                         COUNT(CASE WHEN `enrolled_via_dep` = '0' THEN 1 END) AS No
                         FROM `jamf`
                         LEFT JOIN reportdata USING (serial_number)
                         ".get_machine_group_filter();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+
+        $queryobj = new Jamf_model();
+        jsonView(current($queryobj->query($sql)));
     }
 
     /**
@@ -44,14 +54,14 @@ class jamf_controller extends Module_controller
     **/
     public function get_user_approved_enrollment()
     {
-        $obj = new View();
-        $queryobj = new Jamf_model();
         $sql = "SELECT  COUNT(CASE WHEN `user_approved_enrollment` = '1' THEN 1 END) AS Yes,
                         COUNT(CASE WHEN `user_approved_enrollment` = '0' THEN 1 END) AS No
                         FROM `jamf`
                         LEFT JOIN reportdata USING (serial_number)
                         ".get_machine_group_filter();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+
+        $queryobj = new Jamf_model();
+        jsonView(current($queryobj->query($sql)));
     }
 
     /**
@@ -61,14 +71,14 @@ class jamf_controller extends Module_controller
     **/
     public function get_user_approved_mdm()
     {
-        $obj = new View();
-        $queryobj = new Jamf_model();
         $sql = "SELECT  COUNT(CASE WHEN `user_approved_mdm` = '1' THEN 1 END) AS Yes,
                         COUNT(CASE WHEN `user_approved_mdm` = '0' THEN 1 END) AS No
                         FROM `jamf`
                         LEFT JOIN reportdata USING (serial_number)
                         ".get_machine_group_filter();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+
+        $queryobj = new Jamf_model();
+        jsonView(current($queryobj->query($sql)));
     }
 
     /**
@@ -78,14 +88,14 @@ class jamf_controller extends Module_controller
     **/
     public function get_mdm_capable()
     {
-        $obj = new View();
-        $queryobj = new Jamf_model();
         $sql = "SELECT  COUNT(CASE WHEN `mdm_capable` = '1' THEN 1 END) AS Yes,
                         COUNT(CASE WHEN `mdm_capable` = '0' THEN 1 END) AS No
                         FROM `jamf`
                         LEFT JOIN reportdata USING (serial_number)
                         ".get_machine_group_filter();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+
+        $queryobj = new Jamf_model();
+        jsonView(current($queryobj->query($sql)));
     }
 
     /**
@@ -95,14 +105,14 @@ class jamf_controller extends Module_controller
     **/
     public function get_purchased_leased()
     {
-        $obj = new View();
-        $queryobj = new Jamf_model();
         $sql = "SELECT  COUNT(CASE WHEN `is_purchased` = '1' THEN 1 END) AS Purchased,
                         COUNT(CASE WHEN `is_leased` = '1' THEN 1 END) AS Leased
                         FROM `jamf`
                         LEFT JOIN reportdata USING (serial_number)
                         ".get_machine_group_filter();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+
+        $queryobj = new Jamf_model();
+        jsonView(current($queryobj->query($sql)));
     }
 
     /**
@@ -112,15 +122,15 @@ class jamf_controller extends Module_controller
     **/
     public function get_pending_failed()
     {
-        $obj = new View();
-        $queryobj = new Jamf_model();
         $sql = "SELECT  COUNT(CASE WHEN `comands_completed` = '1' THEN 1 END) AS Completed,
                         COUNT(CASE WHEN `comands_pending` = '1' THEN 1 END) AS Pending,
                         COUNT(CASE WHEN `comands_failed` = '1' THEN 1 END) AS Failed
                         FROM `jamf`
                         LEFT JOIN reportdata USING (serial_number)
                         ".get_machine_group_filter();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+
+        $queryobj = new Jamf_model();
+        jsonView(current($queryobj->query($sql)));
     }
 
     /**
@@ -130,14 +140,14 @@ class jamf_controller extends Module_controller
     **/
     public function get_automatic_login_disabled()
     {
-        $obj = new View();
-        $queryobj = new Jamf_model();
         $sql = "SELECT  COUNT(CASE WHEN `disable_automatic_login` = '1' THEN 1 END) AS Yes,
                         COUNT(CASE WHEN `disable_automatic_login` = '0' THEN 1 END) AS No
                         FROM `jamf`
                         LEFT JOIN reportdata USING (serial_number)
                         ".get_machine_group_filter();
-        $obj->view('json', array('msg' => current($queryobj->query($sql))));
+
+        $queryobj = new Jamf_model();
+        jsonView(current($queryobj->query($sql)));
     }
 
     /**
@@ -148,9 +158,23 @@ class jamf_controller extends Module_controller
      **/
     public function get_xprotect_version()
     {
-        $obj = new View();
-        $jamf = new Jamf_model;
-        $obj->view('json', array('msg' => $jamf->get_xprotect_version()));
+        $sql = "SELECT COUNT(CASE WHEN xprotect_version <> '' AND xprotect_version IS NOT NULL THEN 1 END) AS count, xprotect_version 
+                FROM jamf
+                LEFT JOIN reportdata USING (serial_number)
+                ".get_machine_group_filter()."
+                GROUP BY xprotect_version
+                ORDER BY count DESC";
+
+        $out = [];
+        $queryobj = new Jamf_model;
+        foreach ($queryobj->query($sql) as $obj) {
+            if ("$obj->count" !== "0") {
+                $obj->xprotect_version = $obj->xprotect_version ? $obj->xprotect_version : 'Unknown';
+                $out[] = $obj;
+            }
+        }
+
+        jsonView($out);
     }
     
     /**
@@ -161,9 +185,23 @@ class jamf_controller extends Module_controller
      **/
     public function get_jamf_version()
     {
-        $obj = new View();
-        $jamf = new Jamf_model;
-        $obj->view('json', array('msg' => $jamf->get_jamf_version()));
+        $sql = "SELECT COUNT(CASE WHEN jamf_version <> '' AND jamf_version IS NOT NULL THEN 1 END) AS count, jamf_version 
+                FROM jamf
+                LEFT JOIN reportdata USING (serial_number)
+                ".get_machine_group_filter()."
+                GROUP BY jamf_version
+                ORDER BY count DESC";
+
+        $out = [];
+        $queryobj = new Jamf_model;
+        foreach ($queryobj->query($sql) as $obj) {
+            if ("$obj->count" !== "0") {
+                $obj->jamf_version = $obj->jamf_version ? $obj->jamf_version : 'Unknown';
+                $out[] = $obj;
+            }
+        }
+
+        jsonView($out);
     }
 
     /**
@@ -174,9 +212,23 @@ class jamf_controller extends Module_controller
      **/
     public function get_jamf_departments()
     {
-        $obj = new View();
-        $jamf = new Jamf_model;
-        $obj->view('json', array('msg' => $jamf->get_jamf_departments()));
+        $sql = "SELECT COUNT(CASE WHEN department <> '' AND department IS NOT NULL THEN 1 END) AS count, department
+                FROM jamf
+                LEFT JOIN reportdata USING (serial_number)
+                ".get_machine_group_filter()."
+                GROUP BY department
+                ORDER BY count DESC";
+
+        $out = [];
+        $queryobj = new Jamf_model;
+        foreach ($queryobj->query($sql) as $obj) {
+            if ("$obj->count" !== "0") {
+                $obj->department = $obj->department ? $obj->department : 'Unknown';
+                $out[] = $obj;
+            }
+        }
+
+        jsonView($out);
     }
 
     /**
@@ -187,9 +239,23 @@ class jamf_controller extends Module_controller
      **/
     public function get_jamf_buildings()
     {
-        $obj = new View();
-        $jamf = new Jamf_model;
-        $obj->view('json', array('msg' => $jamf->get_jamf_buildings()));
+        $sql = "SELECT COUNT(CASE WHEN building <> '' AND building IS NOT NULL THEN 1 END) AS count, building
+                FROM jamf
+                LEFT JOIN reportdata USING (serial_number)
+                ".get_machine_group_filter()."
+                GROUP BY building
+                ORDER BY count DESC";
+
+        $out = [];
+        $queryobj = new Jamf_model;
+        foreach ($queryobj->query($sql) as $obj) {
+            if ("$obj->count" !== "0") {
+                $obj->building = $obj->building ? $obj->building : 'Unknown';
+                $out[] = $obj;
+            }
+        }
+
+        jsonView($out);
     }
 
     /**
@@ -199,21 +265,19 @@ class jamf_controller extends Module_controller
      **/
      public function get_last_checkin()
      {        
-         $obj = new View();
-         $queryobj = new Jamf_model();
          $currentdate = date_timestamp_get(date_create());
-         $week = ($currentdate - 604800)*1000;
-         $month = ($currentdate - 2592000)*1000;
-        
+         $week = $currentdate - 604800;
+         $month = $currentdate - 2592000;
+
          $sql = "SELECT COUNT( CASE WHEN ".$month." >= last_contact_time_epoch THEN 1 END) AS red,
 						COUNT( CASE WHEN ".$week." >= last_contact_time_epoch AND last_contact_time_epoch > ".$month." THEN 1 END) AS yellow,
 						COUNT( CASE WHEN last_contact_time_epoch > ".$week." AND last_contact_time_epoch > 0 THEN 1 END) AS green
 						FROM jamf
                         LEFT JOIN reportdata USING (serial_number)
 						".get_machine_group_filter();
-         
-         $lastcheckin_array = $queryobj->query($sql);
-         $obj->view('json', array('msg' => $lastcheckin_array));
+
+         $queryobj = new Jamf_model();
+         jsonView($queryobj->query($sql));
      }
 
     /**
@@ -224,13 +288,6 @@ class jamf_controller extends Module_controller
      **/
     public function pull_all_jamf_data($incoming_serial = '')
     {
-        $obj = new View();
-        // Authenticate
-        if (! $this->authorized()) {
-            $obj->view('json', array('msg' => array('error' => 'Not authenticated')));
-            return;
-        }
-
         // Check if we are returning a list of all serials or processing a serial
         // Returns either a list of all serial numbers in MunkiReport OR
         // a JSON of what serial number was just ran with the status of the run
@@ -247,12 +304,9 @@ class jamf_controller extends Module_controller
             // Loop through each serial number for processing
             $out = array();
             foreach ($machine->query($sql) as $serialobj) {
-//                print_r('{"serial": "'.$incoming_serial.'","status": "'.$jamf_status.'"}');
-//                $jamf = new Jamf_model($serialobj->serial_number);
-//                $jamf->run_jamf_stats();
                 $out[] = $serialobj->serial_number;
             }
-            $obj->view('json', array('msg' => $out));
+            jsonView($out);
         } else {
             $jamf = new Jamf_model($incoming_serial);
             $jamf_status = $jamf->run_jamf_stats();
@@ -263,7 +317,7 @@ class jamf_controller extends Module_controller
             } else {
                 $out = array("serial"=>$incoming_serial,"status"=>"Machine processed");
             }
-            $obj->view('json', array('msg' => $out));
+            jsonView($out);
         }
     }
 
@@ -290,13 +344,7 @@ class jamf_controller extends Module_controller
      **/
     public function get_data($serial_number = '')
     {
-        $obj = new View();
         $jamf = new Jamf_model($serial_number);
-        $obj->view('json', array('msg' => $jamf->rs));
-    }
-    public function admin()
-    {
-        $obj = new View();
-        $obj->view('jamf_admin', [], $this->module_path.'/views/');
+        jsonView($jamf->rs);
     }
 } // End class jamf_module
